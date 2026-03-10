77d13877de7b61d1ce85be25951dd8489fb1d3dd
hnck0912 <phantritrung1209@gmail.com>
2026-03-10 20:25:31 +0700

    Hoan thanh Tuan 9

--- src/main/java/ikus/config/SecurityConfig.java
+++ src/main/java/ikus/config/SecurityConfig.java
@@ -31,11 +31,14 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
 
+                        .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+
                         .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
 
                         .requestMatchers(HttpMethod.POST, "/api/projects/**").hasRole("MANAGER")
 
                         .anyRequest().authenticated()
+
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
